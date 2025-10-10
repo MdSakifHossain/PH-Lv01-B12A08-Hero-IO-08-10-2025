0bd5d6aa59386a2d5c68067425a5493bdcce9686
MdSakifHossain <sakif015sakif@gmail.com>
2025-10-10 10:38:50 +0600

    feat(loader): Add LoadingSpinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import { Toaster } from "sonner";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 const App = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       {/* <Toaster position="top-right" /> */}
-      <div className="min-h-svh bg-bg font-inter flex flex-col items-start">
-        <Header />
-        <main className="flex-1 w-full flex flex-col items-start">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
+      {navigation.state !== "idle" ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="min-h-svh bg-bg font-inter flex flex-col items-start">
+          <Header />
+          <main className="flex-1 w-full flex flex-col items-start">
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+      )}
     </>
   );
 };
