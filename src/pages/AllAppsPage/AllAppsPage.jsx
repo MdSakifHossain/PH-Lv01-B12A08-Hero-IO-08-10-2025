fabc7aef69395041d36850e00cbf86eb71bfd73d
MdSakifHossain <sakif015sakif@gmail.com>
2025-10-10 19:07:07 +0600

    feat(search-feature): Add search functionality on app search

--- src/pages/AllAppsPage/AllAppsPage.jsx
+++ src/pages/AllAppsPage/AllAppsPage.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router";
 import { FiSearch } from "react-icons/fi";
+
 import AppCard from "../../components/AppCard";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 const AllAppsPage = () => {
-  const allApps = useLoaderData().data;
-  console.log(allApps);
+  const { data } = useLoaderData();
+
+  const [allApps, setAllApps] = useState(data);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isSearchEmpty = searchQuery.trim() === "";
+
+  const appsToDisplayedOnThyCanvas = isSearchEmpty
+    ? allApps
+    : allApps.filter((app) =>
+        app.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+
+  const handleQueryChange = (e) => {
+    setIsLoading(true);
+
+    setSearchQuery(e.target.value);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 150);
+  };
 
   return (
     <section className="p-10 md:p-20 flex-1 w-full flex flex-col gap-10">
@@ -21,7 +43,7 @@ const AllAppsPage = () => {
       <div className="flex flex-col gap-4">
         <div className="flex items-start justify-between flex-col md:flex-row gap-4">
           <h4 className="font-semibold text-2xl leading-8 text-heading-color">
-            ({allApps.length}) Apps Found
+            ({isLoading ? "?" : appsToDisplayedOnThyCanvas.length}) Apps Found
           </h4>
 
           <label className="input bg-transparent focus:outline-0 focus-within:outline-0">
@@ -30,13 +52,24 @@ const AllAppsPage = () => {
               type="search"
               className="grow focus:outline-0 focus-within:outline-0"
               placeholder="Search"
+              value={searchQuery}
+              onChange={(e) => handleQueryChange(e)}
             />
           </label>
         </div>
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          {allApps.map((app) => (
-            <AppCard key={app.id} app={app} />
-          ))}
+
+        <div className="w-full">
+          {isLoading ? (
+            <LoadingSpinner />
+          ) : appsToDisplayedOnThyCanvas.length > 0 ? (
+            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+              {appsToDisplayedOnThyCanvas.map((app) => (
+                <AppCard key={app.id} app={app} />
+              ))}
+            </div>
+          ) : (
+            <div className="border">app not found</div>
+          )}
         </div>
       </div>
     </section>
